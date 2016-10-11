e4a4f12b03d5a836349bcd594a8f1261c0e00a4c
Nicholas Costanzo <nicholascostanzo@Nicholass-MacBook-Pro.local>
2016-10-11 12:34:45 -0400

    Added a condition for the cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,9 @@ var cardOne = "Queen";
 var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
+
+if (cardTwo === cardFour) {
+	alert("You have a match!");
+} else {
+	alert("That's not a match :(. Try again!");
+}

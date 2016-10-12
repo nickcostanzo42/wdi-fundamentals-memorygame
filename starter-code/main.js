193812985f18809abad1ce68f1c203e2c72e6cbf
Nicholas Costanzo <nicholascostanzo@Nicholass-MacBook-Pro.local>
2016-10-12 16:29:38 -0400

    created cards using DOM methods

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,27 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+//var cardOne = "Queen";
+//var cardTwo = "Queen";
+//var cardThree = "King";
+//var cardFour = "King";
 
-if (cardTwo === cardFour) {
+/*if (cardTwo === cardFour) {
 	alert("You have a match!");
 } else {
 	alert("That's not a match :(. Try again!");
+}*/
+
+
+var cardsOnBoard = document.getElementById('game-board')
+
+var createCards = function(){
+
+for (var i = 0; i < 4; i++) {
+	var card = document.createElement('div');
+	card.className = 'card';
+	cardsOnBoard.appendChild(card);
 }
+
+}
+
+createCards();
